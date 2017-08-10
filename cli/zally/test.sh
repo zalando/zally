330fac2c99b7e009ebdacb80026ec6477d760f26
Mikhail Chernykh <netmisch@gmail.com>
2017-08-10 22:54:24 +0200

    cli: Add test.sh wrapper to run go tests with coverage #471
    Inspiration: https://github.com/codecov/example-go#caveat-multiple-files
    
    Problem: Go cannot generate coverage reports for multiple packages,
    that's why a small workaround is needed. `test.sh` scans all our
    go packages and runs our test command against them

---
+++ cli/zally/test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+GO_PACKAGES=$(go list ./...)
+RUN_TESTS="go test -coverprofile=cover.out -covermode=atomic -v"
+
+if [ "$1" == "integration" ]; then
+    RUN_TESTS="$RUN_TESTS -tags=integration"
+fi
+
+for package in $GO_PACKAGES; do
+    if ! $RUN_TESTS $package ; then
+        rm cover.out
+        exit $?
+    fi
+    cat cover.out >> coverage.txt
+done
+rm cover.out
+
+echo "======================"
+echo " Code Coverage Report "
+echo "======================"
+cat coverage.txt
