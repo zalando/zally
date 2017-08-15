88ec87f0967cc722ef1dcae649565cfd3f31d82e
Oleg Prozorov <oprozorov@localhost.localdomain>
2017-08-15 15:45:23 +0200

    GH-478 Add server that will be triggered by github webhook

---
+++ github-integration/src/main/java/de/zalando/zally/github/Application.java
@@ -0,0 +1,18 @@
+package de.zalando.zally.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.zalando.twintip.spring.SchemaResource;
+
+@SpringBootApplication
+@Import(SchemaResource.class)
+@EnableScheduling
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
