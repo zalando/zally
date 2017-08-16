874bbef6e83fed0408249034b40153aeb02fa0e2
Alexander Inozemtsev <alexander.inozemtsev@zalando.de>
2017-08-16 10:29:55 +0200

    GH-478 validate payload from github

--- github-integration/src/main/java/de/zalando/zally/github/ApiValidationController.kt
+++ github-integration/src/main/java/de/zalando/zally/github/ApiValidationController.kt
@@ -17,14 +17,16 @@ constructor(private val githubService: GithubService) {
     @ResponseBody
     @PostMapping("/api-validation")
     @ResponseStatus(value = HttpStatus.ACCEPTED)
-    fun validatePullRequest(@RequestBody payload: String, @RequestHeader(value = "X-GitHub-Event") eventType: String) {
+    fun validatePullRequest(@RequestBody payload: String,
+                            @RequestHeader(value = "X-GitHub-Event") eventType: String,
+                            @RequestHeader(value = "X-Hub-Signature") signature: String) {
 
         if (eventType != ApiValidationController.PULL_REQUEST_EVENT_NAME) {
             LOG.info("Received unsupported event: {}", eventType)
             return
         }
 
-        val pullRequest = githubService.parsePayload(payload)
+        val pullRequest = githubService.parsePayload(payload, signature)
 
         pullRequest.updateCommitState(GHCommitState.FAILURE, "https://127.0.0.1", "Hello", "Test")
     }
--- github-integration/src/main/java/de/zalando/zally/github/GithubService.kt
+++ github-integration/src/main/java/de/zalando/zally/github/GithubService.kt
@@ -4,18 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import org.kohsuke.github.GHEventPayload
 import org.kohsuke.github.GitHub
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.crypto.codec.Hex
 import org.springframework.stereotype.Service
 
-import java.io.IOException
 import java.io.StringReader
+import java.security.MessageDigest
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
 
 @Service
 class GithubService
-constructor(private val gitHub: GitHub) {
+constructor(private val gitHub: GitHub, @Value("\${zally.secret}") private val secret: String) {
     private val yamlMapper = ObjectMapper(YAMLFactory())
 
-    fun parsePayload(payload: String): PullRequest {
+    fun parsePayload(payload: String, signature: String): PullRequest {
+        validatePayload(payload, signature)
+
         val pullRequestPayload = gitHub.parseEventPayload(
                 StringReader(payload),
                 GHEventPayload.PullRequest::class.java
@@ -27,5 +33,17 @@ constructor(private val gitHub: GitHub) {
                 .sha
 
         return PullRequest(yamlMapper, pullRequestPayload.repository, commitHash)
+    }
+
+    fun validatePayload(payload: String, signature: String) {
+        val keySpec = SecretKeySpec(secret.toByteArray(),"HmacSHA1")
+
+        val mac = Mac.getInstance("HmacSHA1")
+        mac.init(keySpec)
+        val result = mac.doFinal(payload.toByteArray())
+
+        if (!MessageDigest.isEqual(signature.toByteArray(), "sha1=${String(Hex.encode(result))}".toByteArray())) {
+            throw SecurityException("Signature mismatch")
+        }
     }
 }
