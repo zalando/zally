b115477c16b213c465e20836fdef32a9dc41f6d0
Maxim Tschumak <maxim.tschumak@gmail.com>
2017-09-13 14:57:35 +0200

    fix: removing unused import (#476)

--- server/src/main/java/de/zalando/zally/statistic/ReviewStatistics.java
+++ server/src/main/java/de/zalando/zally/statistic/ReviewStatistics.java
@@ -5,7 +5,6 @@ import de.zalando.zally.apireview.RuleViolation;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class ReviewStatistics {
