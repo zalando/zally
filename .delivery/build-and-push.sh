47707955da0f3ae0ee7e6951b783115ef142da88
Maxim Tschumak <maxim.tschumak@gmail.com>
2017-09-21 08:31:31 +0200

    initial CDP integration (#516)

--- .delivery/build-and-push.sh
+++ .delivery/build-and-push.sh
@@ -1,27 +1,47 @@
 #!/usr/bin/env bash
-# This CDP script builds all the components and runs unit and integration tests
+# This CDP script builds and pushes Docker images
 
 readonly DOCKER_HOST="pierone.stups.zalan.do"
 readonly DOCKER_TEAM="architecture"
 readonly DOCKER_VERSION="${CDP_TARGET_BRANCH}-${CDP_TARGET_COMMIT_ID:0:8}-${CDP_TARGET_REPOSITORY_COUNTER}"
 readonly IS_PR_BUILD=${CDP_PULL_REQUEST_NUMBER+true}
 readonly IS_CDP_BUILD=${CDP_TARGET_BRANCH+true}
-
-REPO_ROOT=$PWD
+readonly REPO_ROOT=$PWD
 
 if [ "$IS_PR_BUILD" = true ]; then
   echo "We're in a pull request, aborting."
   exit 0
 fi
 
-#TODO Build docker images only of the components which were changed
+# Get files which have been changed
 
-# Build and push Zally Server Docker image
-cd ${REPO_ROOT}/server
-docker build -t "${DOCKER_HOST}/${DOCKER_TEAM}/zally:${DOCKER_VERSION}" .
-docker push "${DOCKER_HOST}/${DOCKER_TEAM}/zally:${DOCKER_VERSION}"
+changed_files=($(curl -s https://api.github.com/repos/zalando-incubator/zally/pulls/${CDP_PULL_REQUEST_NUMBER}/files | jq '.[] | .filename' | tr -d "\""))
 
-# Build and push Zally Web UI dummy image
-cd ${REPO_ROOT}/web-ui
-docker build -t "${DOCKER_HOST}/${DOCKER_TEAM}/zally-web-ui-dummy:${DOCKER_VERSION}" .
-docker push "${DOCKER_HOST}/${DOCKER_TEAM}/zally-web-ui-dummy:${DOCKER_VERSION}"
+server_changed=false
+web_ui_changed=false
+
+for f in "${changed_files[@]}"
+do
+  if [[ $f == server/* ]]; then
+    server_changed=true
+  fi
+  if [[ $f == web-ui/* ]]; then
+    web_ui_changed=true
+  fi
+done
+
+# Build and push Docker images
+
+if [ "$server_changed" = true ]; then
+  echo "Building and pushing Zally Server"
+  cd ${REPO_ROOT}/server
+  docker build -t "${DOCKER_HOST}/${DOCKER_TEAM}/zally:${DOCKER_VERSION}" .
+  docker push "${DOCKER_HOST}/${DOCKER_TEAM}/zally:${DOCKER_VERSION}"
+fi
+
+if [ "$web_ui_changed" = true ]; then
+  echo "Building and pushing Zally Web UI Dummy"
+  cd ${REPO_ROOT}/web-ui
+  docker build -t "${DOCKER_HOST}/${DOCKER_TEAM}/zally-web-ui-dummy:${DOCKER_VERSION}" .
+  docker push "${DOCKER_HOST}/${DOCKER_TEAM}/zally-web-ui-dummy:${DOCKER_VERSION}"
+fi
