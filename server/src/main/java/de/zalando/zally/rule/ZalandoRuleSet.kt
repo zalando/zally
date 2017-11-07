d7a10e61fdc9776a55e96dd45cf088596c424aac
Rob Oxspring <roxspring@imapmail.org>
2017-11-07 21:14:55 +0000

    Commented RuleSet interface

--- server/src/main/java/de/zalando/zally/rule/ZalandoRuleSet.kt
+++ server/src/main/java/de/zalando/zally/rule/ZalandoRuleSet.kt
@@ -1,10 +1,11 @@
 package de.zalando.zally.rule
 
 import org.springframework.stereotype.Component
+import java.net.URI
 
 @Component
 class ZalandoRuleSet : RuleSet {
     override val id = javaClass.simpleName
     override val title = "Zalando RESTful API and Event Scheme Guidelines"
-    override val url = "https://zalando.github.io/restful-api-guidelines/"
+    override val url = URI.create("https://zalando.github.io/restful-api-guidelines/")
 }
--- server/src/main/java/de/zalando/zally/rule/ZallyRuleSet.kt
+++ server/src/main/java/de/zalando/zally/rule/ZallyRuleSet.kt
@@ -1,10 +1,11 @@
 package de.zalando.zally.rule
 
 import org.springframework.stereotype.Component
+import java.net.URI
 
 @Component
 class ZallyRuleSet : RuleSet {
     override val id = javaClass.simpleName
     override val title = "Additional Zally Swagger Rules"
-    override val url = "https://github.com/zalando-incubator/zally"
+    override val url = URI.create("https://github.com/zalando-incubator/zally")
 }
