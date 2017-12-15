ecf432023e8aa1e5c601d5f4ca6355b3b4748e31
Rui Araújo <ruka.araujo@gmail.com>
2017-12-15 11:56:27 +0100

    Improve webpack configs. Move to yarn.

---
+++ web-ui/webpack/common.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+const pkg = require('../package.json');
+
+const BUILD_DIR = path.resolve(__dirname, '../src/client/public/assets');
+const APP_DIR = path.resolve(__dirname, '../src/client/app');
+
+const config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        include: APP_DIR,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin(`
+@name ${pkg.name}
+@version ${pkg.version}
+@license ${pkg.license}
+    `),
+  ],
+};
+
+module.exports = config;
