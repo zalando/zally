c98f6528033ee1c5d82641204c45fb303dd4c78e
Robert James Oxspring <rjo@corefiling.com>
2017-12-19 11:50:23 +0000

    PUT operations require body parameter

---
+++ server/src/test/java/com/corefiling/zally/rule/operations/PutOperationsRequireBodyParameterTest.kt
@@ -0,0 +1,88 @@
+package com.corefiling.zally.rule.operations
+
+import com.corefiling.zally.rule.CoreFilingRuleSet
+import io.swagger.models.Swagger
+import io.swagger.parser.SwaggerParser
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class PutOperationsRequireBodyParameterTest {
+
+    val cut = PutOperationsRequireBodyParameter(CoreFilingRuleSet())
+
+    @Test
+    fun withEmptyReturnsNull() {
+        Assertions.assertThat(cut.validate(Swagger())).isNull()
+    }
+
+    @Test
+    fun withHealthyPutReturnsNull() {
+        val yaml = """
+            swagger: '2.0'
+            info:
+              title: API Title
+              version: 1.0.0
+            paths:
+              '/things':
+                put:
+                  parameters:
+                    - in: body
+                      name: thing
+                      required: true
+                      schema:
+                        type: array
+                        items:
+                          type: string
+                  responses:
+                    200:
+                      description: Done
+            """.trimIndent()
+        Assertions.assertThat(cut.validate(SwaggerParser().parse(yaml))).isNull()
+    }
+
+    @Test
+    fun withMissingPutReturnsViolation() {
+        val yaml = """
+            swagger: '2.0'
+            info:
+              title: API Title
+              version: 1.0.0
+            paths:
+              '/things':
+                put:
+                  responses:
+                    200:
+                      description: Done
+            """.trimIndent()
+
+        Assertions.assertThat(cut.validate(SwaggerParser().parse(yaml))!!.paths)
+                .hasSameElementsAs(listOf("/things PUT body parameter is missing!"))
+    }
+
+    @Test
+    fun withOptionalPutReturnsViolation() {
+        val yaml = """
+            swagger: '2.0'
+            info:
+              title: API Title
+              version: 1.0.0
+            paths:
+              '/things':
+                put:
+                  parameters:
+                    - in: body
+                      name: thing
+                      required: false
+                      schema:
+                        type: array
+                        items:
+                          type: string
+                  responses:
+                    200:
+                      description: Done
+            """.trimIndent()
+
+        Assertions.assertThat(cut.validate(SwaggerParser().parse(yaml))!!.paths)
+                .hasSameElementsAs(listOf("/things PUT body parameter 'thing' is optional!"))
+    }
+}
