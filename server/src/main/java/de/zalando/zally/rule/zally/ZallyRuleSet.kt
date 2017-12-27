7e22e33ddd54b86a51f59d20c7468d40b913ccd8
Mikhail Chernykh <netmisch@gmail.com>
2017-12-27 11:42:24 +0100

    server: Update URL in ZallyRuleSet #615

--- server/src/main/java/de/zalando/zally/rule/zally/ZallyRuleSet.kt
+++ server/src/main/java/de/zalando/zally/rule/zally/ZallyRuleSet.kt
@@ -9,7 +9,7 @@ import java.net.URI
 class ZallyRuleSet : RuleSet {
     override val id = javaClass.simpleName
     override val title = "Additional Zally Swagger Rules"
-    override val url = URI.create("https://github.com/zalando-incubator/zally/blob/master/server/rules.md")
+    override val url = URI.create("https://github.com/zalando/zally/blob/master/server/rules.md")
     override fun url(rule: Rule): URI {
         val ref = "${rule.id}: ${rule.title}"
                 .toLowerCase()
