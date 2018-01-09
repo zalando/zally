3620c0477072274f4b96b614d20d15111abd8c9e
Robert James Oxspring <rjo@corefiling.com>
2018-01-09 21:52:24 +0000

    CoreFilingRuleSet now resolves urls to rule specific fragments on a single page

--- server/src/main/java/com/corefiling/zally/rule/CoreFilingRuleSet.kt
+++ server/src/main/java/com/corefiling/zally/rule/CoreFilingRuleSet.kt
@@ -7,25 +7,11 @@ import java.net.URI
 
 @Component
 class CoreFilingRuleSet : RuleSet {
-    override val id = code(javaClass)
+    override val id = javaClass.simpleName
     override val title = "CoreFiling API Guidelines"
     override val url = URI.create("https://wiki.int.corefiling.com/platform/APIGuidelines")
 
     override fun url(rule: Rule): URI {
-        return URI.create(url.toString() + page(javaClass) + "#" + code(javaClass))
+        return URI.create(url.toString() + "#" + rule.id)
     }
-}
-
-fun <T> code(clazz: Class<T>): String {
-    return clazz.simpleName
-}
-
-fun <T> page(clazz: Class<T>): String {
-    val name = lastSubpackage(clazz)
-    return name.substring(0, 1).toUpperCase() + name.substring(1)
-}
-
-fun <T> lastSubpackage(clazz: Class<T>): String {
-    val name = clazz.`package`.name
-    return name.substring(name.lastIndexOf('.') + 1)
 }
---
+++ server/src/test/java/com/corefiling/zally/rule/CoreFilingRuleSetTest.kt
@@ -0,0 +1,35 @@
+package com.corefiling.zally.rule
+
+import com.corefiling.zally.rule.resources.SlashesAtEnd
+import de.zalando.zally.rule.api.Rule
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class CoreFilingRuleSetTest {
+
+    val cut = CoreFilingRuleSet()
+
+    @Test
+    fun urlBaseHost() {
+        assertEquals("wiki.int.corefiling.com", cut.url.host)
+    }
+
+    @Test
+    fun urlBasePath() {
+        assertEquals("/platform/APIGuidelines", cut.url.path)
+    }
+
+    @Test
+    fun urlRuleBasePath() {
+        val rule = SlashesAtEnd::class.java.getAnnotation(Rule::class.java)
+
+        assertEquals("/platform/APIGuidelines", cut.url(rule).path)
+    }
+
+    @Test
+    fun urlRuleFragment() {
+        val rule = SlashesAtEnd::class.java.getAnnotation(Rule::class.java)
+
+        assertEquals("SlashesAtEnd", cut.url(rule).fragment)
+    }
+}
