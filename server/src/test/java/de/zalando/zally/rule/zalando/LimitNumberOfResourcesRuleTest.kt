e598a575a28bffd2fc0abe1b1707780dac2ba83c
Robert James Oxspring <rjo@corefiling.com>
2018-01-15 16:54:31 +0000

    LimitNumberOfResourcesRule now caps at 10 rather than 8.

--- server/src/test/java/de/zalando/zally/rule/zalando/LimitNumberOfResourcesRuleTest.kt
+++ server/src/test/java/de/zalando/zally/rule/zalando/LimitNumberOfResourcesRuleTest.kt
@@ -22,13 +22,15 @@ class LimitNumberOfResourcesRuleTest {
         assertThat(result.paths).hasSameElementsAs(listOf(
             "/items",
             "/items/{item_id}",
-            "/items10",
             "/items3",
             "/items4",
             "/items5",
             "/items6",
             "/items7",
             "/items8",
-            "/items9"))
+            "/items9",
+            "/items10",
+            "/items11",
+            "/items12"))
     }
 }
