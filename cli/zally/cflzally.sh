d78db4993aaaec3fc56816ce29cd93b05f391254
Robert James Oxspring <rjo@corefiling.com>
2018-02-22 10:47:59 +0000

    cflzally.sh script now comments a thumbs-up emoji when commit looks good

--- cli/zally/cflzally.sh
+++ cli/zally/cflzally.sh
@@ -7,25 +7,26 @@ echo "Linting $PATH..."
 /usr/bin/zally lint --json $PATH > /tmp/zally.json
 RESULT=$?
 
+echo "Formatting markdown comment..."
 if /usr/bin/jq --exit-status ".violations | length > 0" /tmp/zally.json; then
-
-    echo "Formatting markdown comment..."
     /usr/bin/jq \
         --raw-output \
         "$JSONTOMARKDOWN" \
         /tmp/zally.json > /tmp/zally.md
     /bin/sed -e s/^/\ \ /g /tmp/zally.md
-
-    echo "Commenting on gitlab commit..."
-    /usr/bin/curl \
-        --silent \
-        --request POST \
-        --header "PRIVATE-TOKEN: ${ZALLY_GITLAB_TOKEN}" \
-        --form "note=</tmp/zally.md" \
-        --form "path=$PATH" \
-        --form "line=1" \
-        --form "line_type=new" \
-        ${CI_PROJECT_URL}/../../api/v4/projects/${CI_PROJECT_ID}/repository/commits/${CI_BUILD_REF}/comments
+else
+    /bin/echo -e "\U0001F44D" > /tmp/zally.md
 fi
+
+echo "Commenting on gitlab commit..."
+/usr/bin/curl \
+    --silent \
+    --request POST \
+    --header "PRIVATE-TOKEN: ${ZALLY_GITLAB_TOKEN}" \
+    --form "note=</tmp/zally.md" \
+    --form "path=$PATH" \
+    --form "line=1" \
+    --form "line_type=new" \
+    ${CI_PROJECT_URL}/../../api/v4/projects/${CI_PROJECT_ID}/repository/commits/${CI_BUILD_REF}/comments
 
 exit $RESULT
