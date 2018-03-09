39bff5484ea79d08b3e5a4d6a8c0986ab3653af3
Mikhail Chernykh <netmisch@gmail.com>
2018-03-09 21:21:04 +0100

    server: Fix ktlint issues #659

--- server/src/main/java/de/zalando/zally/dto/RuleDTO.kt
+++ server/src/main/java/de/zalando/zally/dto/RuleDTO.kt
@@ -4,10 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import de.zalando.zally.rule.api.Severity
 
 data class RuleDTO(
-
-        var title: String? = null,
-        var type: Severity? = null,
-        var url: String? = null,
-        var code: String? = null,
-        @JsonProperty("is_active") var active: Boolean? = null
+    var title: String? = null,
+    var type: Severity? = null,
+    var url: String? = null,
+    var code: String? = null,
+    @JsonProperty("is_active") var active: Boolean? = null
 )
