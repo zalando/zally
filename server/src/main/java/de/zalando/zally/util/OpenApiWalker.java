378631459abb606dfa9ee6a03d3c67457c085395
Maximilian Fellner <maximilian.fellner@zalando.de>
2018-03-22 13:11:43 +0100

    WIP(server): optimize OpenApiWalker

--- server/src/main/java/de/zalando/zally/util/OpenApiWalker.java
+++ server/src/main/java/de/zalando/zally/util/OpenApiWalker.java
@@ -8,79 +8,105 @@ import java.lang.reflect.Modifier;
 import java.util.*;
 
 public class OpenApiWalker {
-  public static Map<Object, String> walk(Object object, Collection<Class<?>> ignore) {
-    Queue<Object> objects = new LinkedList<>();
-    Queue<String> pointers = new LinkedList<>();  //
-    Map<Object, String> map = new IdentityHashMap<>();
-    objects.add(object);
-    pointers.add("#"); // JSON pointer root
+  private OpenApiWalker() {
+  }
+
+  public static class OpenApiWalkerResult {
+    private final Map<Object, String> pointers;
+
+    private OpenApiWalkerResult(Map<Object, String> pointers) {
+      this.pointers = pointers;
+    }
+
+    public Map<Object, String> getPointers() {
+      return pointers;
+    }
+  }
+
+  private static enum Marker {
+    X_ZALLY_IGNORE("x-zally-ignore");
+
+    final String key;
+
+    Marker(String key) {
+      this.key = key;
+    }
+  }
+
+  public static OpenApiWalkerResult walk(Object object, Collection<Class<?>> ignore) {
+    Deque<Object> objects = new LinkedList<>();
+    Deque<String> pointers = new LinkedList<>();
+    Map<Object, String> pointersMap = new IdentityHashMap<>();
+    objects.push(object);
+    pointers.push("#"); // JSON pointer root
 
     while (!objects.isEmpty()) {
-      Object o = objects.remove();
-      String path = pointers.remove();
-      map.put(o, path);
+      Object o = objects.pop(); // .remove();
+      String path = pointers.pop(); // .remove();
+      pointersMap.put(o, path);
 
-      if (!ignore.contains(o.getClass())) {
-        if (o instanceof String) {
-          continue;
-        } else if (o instanceof Integer) {
-          continue;
-        } else if (o instanceof Float) {
-          continue;
-        } else if (o instanceof Double) {
-          continue;
-        } else if (o instanceof Boolean) {
-          continue;
-        } else if (o instanceof Enum) {
-          continue;
-        } else if (o instanceof Map) {
-          for (Map.Entry<?, ?> entry : ((Map<?, ?>) o).entrySet()) {
-            Object key = entry.getKey();
-            Object value = entry.getValue();
-            if (key instanceof String && value != null) {
-              pointers.add(path + "/" + rfc6901Encode((String) key));
-              objects.add(value);
-            }
+      if (ignore.contains(o.getClass())) {
+        continue;
+      }
+      if (o instanceof String) {
+        continue;
+      } else if (o instanceof Integer) {
+        continue;
+      } else if (o instanceof Float) {
+        continue;
+      } else if (o instanceof Double) {
+        continue;
+      } else if (o instanceof Boolean) {
+        continue;
+      } else if (o instanceof Enum) {
+        continue;
+      } else if (o instanceof Map) {
+        for (Map.Entry<?, ?> entry : ((Map<?, ?>) o).entrySet()) {
+          Object key = entry.getKey();
+          Object value = entry.getValue();
+          if (key instanceof String && value != null) {
+            pointers.push(path.concat("/").concat(rfc6901Encode((String) key)));
+            objects.push(value);
           }
-        } else if (o instanceof List) {
-          List<?> l = (List<?>) o;
-          for (int i = 0; i < l.size(); i += 1) {
-            Object value = l.get(i);
-            if (value != null) {
-              pointers.add(path + "/" + i);
-              objects.add(value);
-            }
+        }
+      } else if (o instanceof List) {
+        List<?> l = (List<?>) o;
+        for (int i = 0; i < l.size(); i += 1) {
+          Object value = l.get(i);
+          if (value != null) {
+            pointers.push(path + "/" + i);
+            objects.push(value);
           }
-        } else {
-          for (Method m : o.getClass().getDeclaredMethods()) {
-            String name = m.getName();
-            // Find all public getter methods.
-            if (name.startsWith("get")
-                && m.getParameterCount() == 0
-                && Modifier.isPublic(m.getModifiers())
-                && !m.isAnnotationPresent(JsonIgnore.class)) {
-              try {
-                Object value = m.invoke(o);
-                if (value != null) {
-                  if (m.isAnnotationPresent(JsonAnyGetter.class)) {
-                    // A `JsonAnyGetter` method is simply a wrapper for nested properties.
-                    // We must not use the method name but re-use the current path.
-                    pointers.add(path);
-                  } else {
-                    pointers.add(path + "/" + getterNameToPathName(name));
-                  }
-                  objects.add(value);
+        }
+      } else {
+        for (Method m : o.getClass().getDeclaredMethods()) {
+          String name = m.getName();
+          // Find all public getter methods.
+          if (name.startsWith("get")
+              && m.getParameterCount() == 0
+              && Modifier.isPublic(m.getModifiers())
+              && !m.isAnnotationPresent(JsonIgnore.class)) {
+            try {
+              Object value = m.invoke(o);
+              if (value != null) {
+                if (m.isAnnotationPresent(JsonAnyGetter.class)) {
+                  // A `JsonAnyGetter` method is simply a wrapper for nested properties.
+                  // We must not use the method name but re-use the current path.
+                  pointers.push(path);
+                } else {
+                  pointers.push(path.concat("/").concat(getterNameToPathName(name)));
                 }
-              } catch (ReflectiveOperationException e) {
-                String message = String.format("Error invoking %s on %s at path %s", name, o.getClass(), path);
-                throw new RuntimeException(message, e);
+                objects.push(value);
               }
+            } catch (ReflectiveOperationException e) {
+              String message = String.format("Error invoking %s on %s at path %s", name, o.getClass(), path);
+              throw new RuntimeException(message, e);
             }
           }
         }
       }
     }
-    return map;
+    return new OpenApiWalkerResult(pointersMap);
   }
 
   // https://tools.ietf.org/html/rfc6901
@@ -89,7 +115,7 @@ public class OpenApiWalker {
   }
 
   private static String getterNameToPathName(String name) {
-    String s = name.replace("get", "");
-    return s.substring(0, 1).toLowerCase() + s.substring(1);
+    String s = name.substring(3); // `get` is first 3 characters
+    return s.substring(0, 1).toLowerCase().concat(s.substring(1));
   }
 }
