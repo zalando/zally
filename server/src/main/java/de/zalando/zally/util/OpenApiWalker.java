0e3f7deba8e57626b6ff8dd95f9864db2e5a4054
Maximilian Fellner <maximilian.fellner@zalando.de>
2018-03-22 16:36:22 +0100

    WIP(server): markers for OpenApiWalker

--- server/src/main/java/de/zalando/zally/util/OpenApiWalker.java
+++ server/src/main/java/de/zalando/zally/util/OpenApiWalker.java
@@ -3,27 +3,90 @@ package de.zalando.zally.util;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.*;
 
+/**
+ * OpenApiWalker traverses a Swagger or OpenApi object.
+ */
 public class OpenApiWalker {
   private OpenApiWalker() {
   }
 
-  public static class OpenApiWalkerResult {
-    private final Map<Object, String> pointers;
+  /**
+   * Meta information for an object node in a Swagger or OpenApi object.
+   */
+  public static class Meta {
+    private final String pointer;
+    private final Marker marker;
+    private final String markerValue;
 
-    private OpenApiWalkerResult(Map<Object, String> pointers) {
-      this.pointers = pointers;
+    private Meta(String pointer, Node marker) {
+      this.pointer = pointer;
+      this.marker = marker != null ? marker.marker : null;
+      this.markerValue = marker != null ? marker.pointer : null;
     }
 
-    public Map<Object, String> getPointers() {
-      return pointers;
+    @NotNull
+    public String getPointer() {
+      return pointer;
+    }
+
+    @Nullable
+    public Marker getMarker() {
+      return marker;
+    }
+
+    @Nullable
+    public String getMarkerValue() {
+      return markerValue;
     }
   }
 
-  private static enum Marker {
+  /**
+   * A stack node for tree-traversal.
+   */
+  private static class Node {
+    final Object object;
+    final String pointer;
+    final boolean skip;
+    final Marker marker;
+
+    Node(Object object, String pointer) {
+      this(object, pointer, false, null);
+    }
+
+    Node(Object object, String pointer, boolean skip) {
+      this(object, pointer, skip, null);
+    }
+
+    Node(Object object, String pointer, Marker marker) {
+      this(object, pointer, false, marker);
+    }
+
+    Node(Object object, String pointer, boolean skip, Marker marker) {
+      this.object = object;
+      this.pointer = pointer;
+      this.skip = skip;
+      this.marker = marker;
+    }
+
+    boolean shouldSkip() {
+      return this.skip;
+    }
+
+    boolean isMarker() {
+      return this.marker != null;
+    }
+  }
+
+  /**
+   * A type of marker for an object node in a Swagger or OpenApi structure.
+   */
+  public static enum Marker {
     X_ZALLY_IGNORE("x-zally-ignore");
 
     final String key;
@@ -33,17 +96,41 @@ public class OpenApiWalker {
     }
   }
 
-  public static OpenApiWalkerResult walk(Object object, Collection<Class<?>> ignore) {
-    Deque<Object> objects = new LinkedList<>();
-    Deque<String> pointers = new LinkedList<>();
-    Map<Object, String> pointersMap = new IdentityHashMap<>();
-    objects.push(object);
-    pointers.push("#"); // JSON pointer root
+  /**
+   * Traverse a Swagger or OpenApi object tree and construct
+   * a map of object nodes to meta information objects.
+   *
+   * @param object Swagger or OpenApi instance.
+   * @param ignore List of classes in the traversable object that should be ignored.
+   * @return IdentityHashMap of object nodes to meta information objects.
+   */
+  public static Map<Object, Meta> walk(Object object, Collection<Class<?>> ignore) {
+    Deque<Node> nodes = new LinkedList<>(); // stack of tree nodes
+    Deque<Node> markers = new LinkedList<>(); // stack of markers for sub-trees
+    Map<Object, Meta> map = new IdentityHashMap<>(); // map of node objects to JSON pointers
+    nodes.push(new Node(object, "#")); // add the root node
 
-    while (!objects.isEmpty()) {
-      Object o = objects.pop(); // .remove();
-      String path = pointers.pop(); // .remove();
-      pointersMap.put(o, path);
+    while (!nodes.isEmpty()) {
+      Node node = nodes.pop(); // pop the current node
+      Node marker = markers.peek(); // set the current marker
+
+      if (node.isMarker()) {
+        marker = null; // clear the marker
+        markers.pop(); // remove the marker
+        if (nodes.isEmpty()) {
+          continue;
+        } else {
+          node = nodes.pop(); // get the next node
+        }
+      }
+
+      final Object o = node.object;
+      final String p = node.pointer;
+
+      // Add the current node.
+      if (!node.shouldSkip()) {
+        map.put(o, new Meta(p, marker));
+      }
 
       if (ignore.contains(o.getClass())) {
         continue;
@@ -65,8 +152,8 @@ public class OpenApiWalker {
           Object key = entry.getKey();
           Object value = entry.getValue();
           if (key instanceof String && value != null) {
-            pointers.push(path.concat("/").concat(rfc6901Encode((String) key)));
-            objects.push(value);
+            String pointer = p.concat("/").concat(rfc6901Encode((String) key));
+            nodes.push(new Node(value, pointer));
           }
         }
       } else if (o instanceof List) {
@@ -74,11 +161,18 @@ public class OpenApiWalker {
         for (int i = 0; i < l.size(); i += 1) {
           Object value = l.get(i);
           if (value != null) {
-            pointers.push(path + "/" + i);
-            objects.push(value);
+            String pointer = p.concat("/").concat(String.valueOf(i));
+            nodes.push(new Node(value, pointer));
           }
         }
       } else {
+        // Check if an ignore extension exists at this position.
+        String ignoreExtension = getVendorExtension(o, Marker.X_ZALLY_IGNORE.key);
+        if (ignoreExtension != null) {
+          marker = new Node(ignoreExtension, ignoreExtension, Marker.X_ZALLY_IGNORE); // Set the current marker.
+          markers.push(marker); // Add the marker to the marker stack.
+          nodes.push(marker); // Use a node to mark the position on the node stack.
+        }
         for (Method m : o.getClass().getDeclaredMethods()) {
           String name = m.getName();
           // Find all public getter methods.
@@ -91,22 +185,22 @@ public class OpenApiWalker {
               if (value != null) {
                 if (m.isAnnotationPresent(JsonAnyGetter.class)) {
                   // A `JsonAnyGetter` method is simply a wrapper for nested properties.
-                  // We must not use the method name but re-use the current path.
-                  pointers.push(path);
+                  // We must not use the method name but re-use the current pointer.
+                  nodes.push(new Node(value, p, /* skip */true));
                 } else {
-                  pointers.push(path.concat("/").concat(getterNameToPathName(name)));
+                  String pointer = p.concat("/").concat(getterNameToPathName(name));
+                  nodes.push(new Node(value, pointer));
                 }
-                objects.push(value);
               }
             } catch (ReflectiveOperationException e) {
-              String message = String.format("Error invoking %s on %s at path %s", name, o.getClass(), path);
+              String message = String.format("Error invoking %s on %s at path %s", name, o.getClass(), p);
               throw new RuntimeException(message, e);
             }
           }
         }
       }
     }
-    return new OpenApiWalkerResult(pointersMap);
+    return map;
   }
 
   // https://tools.ietf.org/html/rfc6901
@@ -117,5 +211,18 @@ public class OpenApiWalker {
   private static String getterNameToPathName(String name) {
     String s = name.substring(3); // `get` is first 3 characters
     return s.substring(0, 1).toLowerCase().concat(s.substring(1));
+  }
+
+  private static String getVendorExtension(Object object, String extension) {
+    try {
+      Method m = object.getClass().getMethod("getVendorExtensions");
+      Object extensions = m.invoke(object);
+      if (extensions instanceof Map) {
+        return (String) ((Map) extensions).get(extension);
+      }
+    } catch (ReflectiveOperationException e) {
+      return null;
+    }
+    return null;
   }
 }
--- server/src/test/java/de/zalando/zally/util/OpenApiWalkerTest.kt
+++ server/src/test/java/de/zalando/zally/util/OpenApiWalkerTest.kt
@@ -7,18 +7,94 @@ import org.junit.Test
 import java.util.*
 
 class OpenApiWalkerTest {
+    private val ignore = HashSet(Arrays.asList(
+            io.swagger.models.ArrayModel::class.java,
+            io.swagger.models.ComposedModel::class.java,
+            io.swagger.models.ModelImpl::class.java
+    )) as Collection<Class<*>>?
+
     @Test
-    fun `walk Swagger object`() {
+    fun `create Swagger 2 JSON pointers`() {
+        val content = """
+            {
+              "swagger": "2.0",
+              "info": {
+                "title": "Things API",
+                "description": "Description of things",
+                "version": "1.0.0"
+              },
+              "paths": {
+                "/tests": {
+                  "get": {
+                    "responses": {
+                      "200": {
+                        "description": "OK"
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            """.trimIndent()
+
+        val spec = SwaggerParser().parse(content)
+        val map = OpenApiWalker.walk(spec, ignore)
+        assertThat(map).hasSize(13)
+
+        val description = spec.paths?.get("/tests")?.get?.responses?.get("200")?.description
+        assertThat(map[description]?.pointer).isEqualTo("#/paths/~1tests/get/responses/200/description")
+    }
+
+    @Test
+    fun `add Swagger 2 ignore marker`() {
+        val content = """
+            {
+              "swagger": "2.0",
+              "info": {
+                "title": "Things API",
+                "description": "Description of things",
+                "version": "1.0.0"
+              },
+              "paths": {
+                "/tests": {
+                  "x-zally-ignore": "*",
+                  "get": {
+                    "responses": {
+                      "200": {
+                        "description": "OK"
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            """.trimIndent()
+
+        val spec = SwaggerParser().parse(content)
+        val map = OpenApiWalker.walk(spec, ignore)
+        assertThat(map).hasSize(14)
+
+        val description = spec.paths?.get("/tests")?.get?.responses?.get("200")?.description
+        assertThat(map[description]?.marker).isEqualTo(OpenApiWalker.Marker.X_ZALLY_IGNORE)
+        assertThat(map[description]?.markerValue).isEqualTo("*")
+
+        val get = spec.paths?.get("/tests")?.get
+        assertThat(map[get]?.marker).isEqualTo(OpenApiWalker.Marker.X_ZALLY_IGNORE)
+        assertThat(map[get]?.markerValue).isEqualTo("*")
+
+        val testsPath = spec.paths?.get("/tests")
+        assertThat(map[testsPath]?.marker).isNull()
+        assertThat(map[testsPath]?.markerValue).isNull()
+    }
+
+
+    @Test
+    fun `walk Swagger document`() {
         val content = resourceToString("fixtures/api_spp.json")
-        val obj = SwaggerParser().parse(content)
-        val ignore = HashSet(Arrays.asList(
-                io.swagger.models.ArrayModel::class.java,
-                io.swagger.models.ComposedModel::class.java,
-                io.swagger.models.ModelImpl::class.java
-        ))
-        val map = OpenApiWalker.walk(obj, ignore as Collection<Class<*>>?)
-        assertThat(map).isNotEmpty
-        println(map.values.joinToString("\n"))
-        println("mapped ${map.size} paths")
+        val map = SwaggerParser().parse(content)
+        val result = OpenApiWalker.walk(map, ignore)
+        assertThat(result).isNotEmpty
+        // println(result.values.map { "${it.pointer} ${it.markerValue}" }.joinToString("\n"))
+        // println("mapped ${result.size} paths")
     }
 }
