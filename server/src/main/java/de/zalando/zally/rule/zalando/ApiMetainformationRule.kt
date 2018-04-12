f0e79b926d22e6f37dbdd50c9879df43e98a0aa8
Vadim <5587834+vadeg@users.noreply.github.com>
2018-04-12 20:16:18 +0200

    Fixes #636: Fix review comments
    * Version regex updated
    * Validation split into several methods.

--- server/src/main/java/de/zalando/zally/rule/zalando/ApiMetainformationRule.kt
+++ server/src/main/java/de/zalando/zally/rule/zalando/ApiMetainformationRule.kt
@@ -4,8 +4,6 @@ import de.zalando.zally.rule.api.Check
 import de.zalando.zally.rule.api.Rule
 import de.zalando.zally.rule.api.Severity
 import de.zalando.zally.rule.api.Violation
-import io.swagger.models.Contact
-import io.swagger.models.Info
 import io.swagger.models.Swagger
 
 @Rule(
@@ -16,57 +14,67 @@ import io.swagger.models.Swagger
 )
 class ApiMetainformationRule {
 
-    private val versionRegex = """^\d+.\d+.\d+-?[\w+]*""".toRegex()
+    private val versionRegex = """^\d+.\d+(.\d+)?""".toRegex()
     private val basePath = "/info"
-    private val noInfoBlock = "Info block should be provided"
 
-    @Check(severity = Severity.SHOULD)
-    fun validate(swagger: Swagger): Violation? {
+    @Check(severity = Severity.MUST)
+    fun validateInfoBlock(swagger: Swagger): Violation? {
         val info = swagger.info
-        return when {
-            info != null -> validateInfo(info)
-            else -> Violation(noInfoBlock, listOf(basePath))
-        }
-    }
-
-    private fun validateInfo(info: Info): Violation? {
-
-        if (info.title.isNullOrBlank()) {
-            return Violation("Title is not defined", listOf("$basePath/title"))
-        }
-        if (info.description.isNullOrBlank()) {
-            return Violation("Description is not defined", listOf("$basePath/description"))
-        }
-
-        val versionValidation = checkVersion(info.version)
-        if (versionValidation != null) {
-            return versionValidation
-        }
-        return checkContact(info.contact)
-    }
-
-    private fun checkContact(contact: Contact?): Violation? =
-        when {
-            contact == null ->
-                Violation("Contacts are not provided", listOf("$basePath/contact"))
-            contact.name.isNullOrBlank() ->
-                Violation("Contact name is empty", listOf("$basePath/contact/name"))
-            contact.url.isNullOrBlank() ->
-                Violation("Contact url is empty", listOf("$basePath/contact/url"))
-            contact.email.isNullOrBlank() ->
-                Violation("Contact email is empty", listOf("$basePath/contact/email"))
+        return when (info) {
+            null -> Violation("Info block should be provided", listOf(basePath))
             else -> null
         }
+    }
 
+    @Check(severity = Severity.MUST)
+    fun validateInfoTitle(swagger: Swagger): Violation? =
+        if (swagger.info?.title.isNullOrBlank()) {
+            Violation("Title is not defined", listOf("$basePath/title"))
+        } else null
 
-    private fun checkVersion(version: String?): Violation? =
-        when {
+    @Check(severity = Severity.MUST)
+    fun validateInfoDescription(swagger: Swagger): Violation? =
+        if (swagger.info?.description.isNullOrBlank()) {
+            Violation("Description is not defined", listOf("$basePath/description"))
+        } else null
+
+    @Check(severity = Severity.MUST)
+    fun validateInfoVersion(swagger: Swagger): Violation? {
+        val version = swagger.info?.version
+        return when {
             version.isNullOrBlank() ->
                 Violation("Version is not defined", listOf("$basePath/version"))
             !versionRegex.matches(version!!) ->
                 Violation("Version is not following Semver rules", listOf("$basePath/version"))
             else -> null
         }
+    }
+
+    @Check(severity = Severity.MUST)
+    fun validateContact(swagger: Swagger): Violation? =
+        when (swagger.info?.contact) {
+            null -> Violation("Contacts are not provided", listOf("$basePath/contact"))
+            else -> null
+        }
 
 
+    @Check(severity = Severity.MUST)
+    fun validateContactName(swagger: Swagger): Violation? =
+        if (swagger.info?.contact?.name.isNullOrBlank()) {
+            Violation("Contact name is empty", listOf("$basePath/contact/name"))
+        } else null
+
+
+    @Check(severity = Severity.MUST)
+    fun validateContactUrl(swagger: Swagger): Violation? =
+        if (swagger.info?.contact?.url.isNullOrBlank()) {
+            Violation("Contact url is empty", listOf("$basePath/contact/url"))
+        } else null
+
+
+    @Check(severity = Severity.MUST)
+    fun validateContactEmail(swagger: Swagger): Violation? =
+        if (swagger.info?.contact?.email.isNullOrBlank()) {
+            Violation("Contact email is empty", listOf("$basePath/contact/email"))
+        } else null
 }
--- server/src/test/java/de/zalando/zally/rule/zalando/ApiMetainformationRuleTest.kt
+++ server/src/test/java/de/zalando/zally/rule/zalando/ApiMetainformationRuleTest.kt
@@ -12,16 +12,15 @@ class ApiMetainformationRuleTest {
     val swagger = getFixture("218.yaml")
 
     @Test
-    fun validationPassed() {
-        val violation = rule.validate(swagger)
-        assertThat(violation).isNull()
-    }
+    fun validationPassed() =
+        assertThat(validate(swagger).filterNotNull()).isEmpty()
+
 
     @Test
     fun infoBlockNotFound() {
         val expected = Violation("Info block should be provided", listOf("/info"))
         val emptyDefinition = Swagger()
-        val violation = rule.validate(emptyDefinition)
+        val violation = rule.validateInfoBlock(emptyDefinition)
         assertThat(violation).isEqualTo(expected)
     }
 
@@ -29,7 +28,7 @@ class ApiMetainformationRuleTest {
     fun checkInvalidTitle() {
         val expected = Violation("Title is not defined", listOf("/info/title"))
         swagger.info?.title = ""
-        val violation = rule.validate(swagger)
+        val violation = rule.validateInfoTitle(swagger)
         assertThat(violation).isEqualTo(expected)
     }
 
@@ -37,7 +36,7 @@ class ApiMetainformationRuleTest {
     fun checkInvalidDescription() {
         val expected = Violation("Description is not defined", listOf("/info/description"))
         swagger.info?.description = ""
-        val violation = rule.validate(swagger)
+        val violation = rule.validateInfoDescription(swagger)
         assertThat(violation).isEqualTo(expected)
     }
 
@@ -45,26 +44,26 @@ class ApiMetainformationRuleTest {
     fun checkVersionNotDefined() {
         val expected = Violation("Version is not defined", listOf("/info/version"))
         swagger.info?.version = ""
-        val violation = rule.validate(swagger)
+        val violation = rule.validateInfoVersion(swagger)
         assertThat(violation).isEqualTo(expected)
     }
 
     @Test
     fun versionHasWrongFormat() {
-        var violation = rule.validate(swagger)
+        var violation = rule.validateInfoVersion(swagger)
         assertThat(violation).isNull()
 
         val expected = Violation("Version is not following Semver rules", listOf("/info/version"))
         swagger.info.version = "abc.32.32"
-        violation = rule.validate(swagger)
+        violation = rule.validateInfoVersion(swagger)
         assertThat(violation).isEqualTo(expected)
 
         swagger.info.version = "1.d.1"
-        violation = rule.validate(swagger)
+        violation = rule.validateInfoVersion(swagger)
         assertThat(violation).isEqualTo(expected)
 
         swagger.info.version = "some version"
-        violation = rule.validate(swagger)
+        violation = rule.validateInfoVersion(swagger)
         assertThat(violation).isEqualTo(expected)
     }
 
@@ -72,26 +71,37 @@ class ApiMetainformationRuleTest {
     fun invalidContact() {
         val contact = swagger.info.contact
         swagger.info.contact = null
-        var violation = rule.validate(swagger)
+        var violation = rule.validateContact(swagger)
         assertThat(violation).isEqualTo(Violation("Contacts are not provided", listOf("/info/contact")))
 
         swagger.info.contact = contact
 
         val oldName = contact.name
         contact.name = ""
-        violation = rule.validate(swagger)
+        violation = rule.validateContactName(swagger)
         assertThat(violation).isEqualTo(Violation("Contact name is empty", listOf("/info/contact/name")))
         contact.name = oldName
 
         val oldUrl = contact.url
         contact.url = ""
-        violation = rule.validate(swagger)
+        violation = rule.validateContactUrl(swagger)
         assertThat(violation).isEqualTo(Violation("Contact url is empty", listOf("/info/contact/url")))
         contact.url = oldUrl
 
         contact.email = ""
-        violation = rule.validate(swagger)
+        violation = rule.validateContactEmail(swagger)
         assertThat(violation).isEqualTo(Violation("Contact email is empty", listOf("/info/contact/email")))
     }
+
+    private fun validate(swagger: Swagger): List<Violation?> =
+        listOf(rule.validateInfoBlock(swagger),
+            rule.validateInfoTitle(swagger),
+            rule.validateInfoDescription(swagger),
+            rule.validateInfoVersion(swagger),
+            rule.validateContact(swagger),
+            rule.validateContactName(swagger),
+            rule.validateContactUrl(swagger),
+            rule.validateContactEmail(swagger))
+
 
 }
