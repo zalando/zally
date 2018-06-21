e73b619c3f6adfafb7f07c4033e41227b79c97d1
Rob Oxspring <roxspring@imapmail.org>
2018-06-21 15:02:17 +0100

    JsonPointers.escape(Method,Args) doesn't change case of arguments #734

--- server/src/test/java/de/zalando/zally/util/ast/JsonPointersTest.kt
+++ server/src/test/java/de/zalando/zally/util/ast/JsonPointersTest.kt
@@ -1,6 +1,7 @@
 package de.zalando.zally.util.ast
 
 import com.fasterxml.jackson.core.JsonPointer
+import io.swagger.models.Swagger
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
@@ -10,5 +11,23 @@ class JsonPointersTest {
         val pointer = JsonPointer.compile("/paths/~1items/get/responses/default/content/application~1json/schema")
         val converted = JsonPointers.convertPointer(pointer)
         assertThat(converted).hasToString("/paths/~1items/get/responses/default/schema")
+    }
+
+    @Test
+    fun escapeWithGetInfoMethod() {
+        val method = Swagger::class.java.methods.first { it.name == "getInfo" }
+
+        val pointer = JsonPointers.escape(method)
+
+        assertThat(pointer).hasToString("/info")
+    }
+
+    @Test
+    fun escapeWithMapGetMethod() {
+        val method = Map::class.java.methods.first { it.name == "get" }
+
+        val pointer = JsonPointers.escape(method, "KEY")
+
+        assertThat(pointer).hasToString("/KEY")
     }
 }
