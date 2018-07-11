e5567b14bc8f25226a8d5b9b26005c0a9df8d6a7
Maxim Tschumak <maxim.tschumak@gmail.com>
2018-07-11 13:10:56 +0200

    feat: functional naming schema checks for hostnames (#749)

--- server/src/main/java/de/zalando/zally/rule/api/Check.kt
+++ server/src/main/java/de/zalando/zally/rule/api/Check.kt
@@ -1,7 +1,7 @@
 package de.zalando.zally.rule.api
 
 @Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FUNCTION)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.FIELD)
 annotation class Check(
     val severity: Severity
 )
