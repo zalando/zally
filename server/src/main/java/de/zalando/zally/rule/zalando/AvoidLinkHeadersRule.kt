70ad4abee238cdee90771a6c68812aab4edbe179
David Dufour-Boivin <david.dufour-boivin@zalando.de>
2018-07-24 17:10:42 +0200

    Litteral YAML in tests + Less class inheritance for rules (#714)

--- server/src/main/java/de/zalando/zally/rule/zalando/AvoidLinkHeadersRule.kt
+++ server/src/main/java/de/zalando/zally/rule/zalando/AvoidLinkHeadersRule.kt
@@ -2,11 +2,12 @@ package de.zalando.zally.rule.zalando
 
 import com.typesafe.config.Config
 import de.zalando.zally.rule.Context
-import de.zalando.zally.rule.HttpHeadersRuleWithContext
+import de.zalando.zally.rule.HttpHeadersRule
 import de.zalando.zally.rule.api.Check
 import de.zalando.zally.rule.api.Rule
 import de.zalando.zally.rule.api.Severity
 import de.zalando.zally.rule.api.Violation
+import de.zalando.zally.util.getAllHeaders
 import org.springframework.beans.factory.annotation.Autowired
 
 @Rule(
@@ -15,16 +16,17 @@ import org.springframework.beans.factory.annotation.Autowired
     severity = Severity.MUST,
     title = "Avoid Link in Header Rule"
 )
-class AvoidLinkHeadersRule(@Autowired rulesConfig: Config) : HttpHeadersRuleWithContext(rulesConfig) {
+class AvoidLinkHeadersRule(@Autowired rulesConfig: Config) {
+
+    private val headersWhitelist = rulesConfig.getStringList(HttpHeadersRule::class.simpleName + ".whitelist").toSet()
+
     private val description = "Do Not Use Link Headers with JSON entities"
 
     @Check(severity = Severity.MUST)
-    override fun validate(context: Context): List<Violation> {
-        return super.validate(context)
+    fun validate(context: Context): List<Violation> {
+        val allHeaders = context.api.getAllHeaders()
+        return allHeaders
+            .filter { it.name !in headersWhitelist && it.name == "Link" }
+            .map { context.violation(description, it.element) } // createViolation(context, it) }
     }
-
-    override fun isViolation(header: HeaderElement) = header.name == "Link"
-
-    override fun createViolation(context: Context, header: HeaderElement): Violation =
-        context.violation(description, header.element)
 }
--- server/src/test/java/de/zalando/zally/rule/zalando/AvoidLinkHeadersRuleTest.kt
+++ server/src/test/java/de/zalando/zally/rule/zalando/AvoidLinkHeadersRuleTest.kt
@@ -1,10 +1,11 @@
 package de.zalando.zally.rule.zalando
 
 import com.fasterxml.jackson.core.JsonPointer
-import de.zalando.zally.getContextFromFixture
+import de.zalando.zally.rule.Context
 import de.zalando.zally.rule.api.Violation
 import de.zalando.zally.testConfig
 import org.assertj.core.api.Assertions.assertThat
+import org.intellij.lang.annotations.Language
 import org.junit.Test
 
 class AvoidLinkHeadersRuleTest {
@@ -12,26 +13,126 @@ class AvoidLinkHeadersRuleTest {
     private val rule = AvoidLinkHeadersRule(testConfig)
 
     @Test
-    fun positiveCaseSpp() {
-        val context = getContextFromFixture("api_spp.json")
+    fun `a Swagger API with no header called Link produces no violation`() {
+        @Language("YAML")
+        val context = Context.createSwaggerContext("""
+            swagger: 2.0
+            info:
+              title: Clean Swagger API
+            paths:
+              /foo:
+                get:
+                  description: Lorem Ipsum
+                  responses:
+                    202:
+                      description: Lorem Ipsum
+                      headers:
+                        Location:
+                          type: string
+                          format: url
+            parameters:
+              FlowId:
+                name: X-Flow-Id
+                in: header
+                type: string
+                required: false
+              Authorization:
+                name: Authorization
+                in: header
+                type: string
+                required: true
+              ProductId:
+                name: product_id
+                in: path
+                type: string
+                required: true
+        """.trimIndent(), failOnParseErrors = true)!!
         val violations = rule.validate(context)
         assertThat(violations).isEmpty()
     }
 
     @Test
-    fun positiveCaseSpa() {
-        val context = getContextFromFixture("api_spa.yaml")
+    fun `an OpenAPI 3 API with no header called Link produces no violation`() {
+        @Language("YAML")
+        val context = Context.createOpenApiContext("""
+            openapi: 3.0.0
+            info:
+              title: Clean Swagger API
+              version: 1.0.0
+            paths:
+              /foo:
+                get:
+                  description: Lorem Ipsum
+                  responses:
+                    202:
+                      description: Lorem Ipsum
+                      headers:
+                        Location:
+                          schema:
+                            type: string
+                            format: url
+            components:
+              parameters:
+                FlowId:
+                  name: X-Flow-Id
+                  in: header
+                  required: false
+                  schema:
+                    type: string
+                Authorization:
+                  name: Authorization
+                  in: header
+                  required: true
+                  schema:
+                    type: string
+                ProductId:
+                  name: product_id
+                  in: path
+                  required: true
+                  schema:
+                    type: string
+        """.trimIndent(), failOnParseErrors = true)!!
         val violations = rule.validate(context)
         assertThat(violations).isEmpty()
     }
 
     @Test
-    fun negativeCase() {
-        val context = getContextFromFixture("avoidLinkHeaderRuleInvalid.json")
+    fun `an API with Link headers causes violations`() {
+        @Language("YAML")
+        val context = Context.createSwaggerContext("""
+            swagger: 2.0
+            info:
+              title: Clean Swagger API
+            paths:
+              /foo:
+                get:
+                  parameters:
+                    - name: Authorization
+                      in: header
+                      type: string
+                    - name: Link
+                      in: header
+                      type: string
+                  responses:
+                    202:
+                      description: Lorem Ipsum
+                      headers:
+                        Location:
+                          type: string
+                          format: url
+                post:
+                  responses:
+                    202:
+                      description: Lorem Ipsum
+                      headers:
+                        Link:
+                          type: string
+                          format: url
+        """.trimIndent(), failOnParseErrors = true)!!
         val violations = rule.validate(context)
         assertThat(violations).hasSameElementsAs(listOf(
-            v("/paths/~1products/get/parameters/4"),
-            v("/paths/~1product-put-requests~1{product_path}/post/responses/202/headers/Link")
+            v("/paths/~1foo/get/parameters/1"),
+            v("/paths/~1foo/post/responses/202/headers/Link")
         ))
     }
 
