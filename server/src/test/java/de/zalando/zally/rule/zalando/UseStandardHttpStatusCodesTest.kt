e20601b159f8e1dd9f96de6bd906e54c98374492
Maxim Tschumak <maxim.tschumak@gmail.com>
2018-07-25 11:35:26 +0200

    feat: add checks for standardized and well-understood http response codes (#752)

--- server/src/test/java/de/zalando/zally/rule/zalando/UseStandardHttpStatusCodesTest.kt
+++ server/src/test/java/de/zalando/zally/rule/zalando/UseStandardHttpStatusCodesTest.kt
@@ -2,16 +2,17 @@ package de.zalando.zally.rule.zalando
 
 import de.zalando.zally.openApiWithOperations
 import de.zalando.zally.rule.DefaultContext
+import de.zalando.zally.rule.api.Context
 import de.zalando.zally.testConfig
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 @Suppress("UndocumentedPublicClass")
 class UseStandardHttpStatusCodesTest {
-    private val codes = UseStandardHttpStatusCodes(testConfig)
+    private val rule = UseStandardHttpStatusCodes(testConfig)
 
     @Test
-    fun shouldPassIfOperationsAreAllowed() {
+    fun `checkWellUnderstoodResponseCodesUsage should return no violations if the usage is correct`() {
         val allowedToAll = listOf(
             "200", "301", "400", "401", "403", "404", "405", "406", "408", "410", "428", "429",
             "500", "501", "503", "default"
@@ -27,11 +28,11 @@ class UseStandardHttpStatusCodesTest {
         val openApi = openApiWithOperations(operations)
         val context = DefaultContext(openApi)
 
-        assertThat(codes.allowOnlyStandardStatusCodes(context)).isEmpty()
+        assertThat(rule.checkWellUnderstoodResponseCodesUsage(context)).isEmpty()
     }
 
     @Test
-    fun shouldNotPassIfOperationsAreNotAllowed() {
+    fun `checkWellUnderstoodResponseCodesUsage should return violation if the well-understood HTTP headeres are used incorrectly`() {
         val notAllowedAll = listOf(
             "203", "205", "206", "208", "226", "302", "305", "306", "307", "308", "402", "407", "411",
             "413", "414", "416", "417", "418", "421", "422", "424", "426", "431", "451", "502", "504",
@@ -53,10 +54,64 @@ class UseStandardHttpStatusCodesTest {
 
         val openApi = openApiWithOperations(operations)
         val context = DefaultContext(openApi)
-        val violations = codes.allowOnlyStandardStatusCodes(context)
+
+        val violations = rule.checkWellUnderstoodResponseCodesUsage(context)
 
         assertThat(violations).isNotEmpty
         assertThat(violations.map { it.pointer.toString() })
             .containsExactlyInAnyOrder(*expectedPointers.toTypedArray())
+    }
+
+    @Test
+    fun `allowOnlyStandardizedResponseCodes should return no violation if standardized response code are used`() {
+        val context = withResponseCode("200")
+
+        val violations = rule.checkIfOnlyStandardizedResponseCodesAreUsed(context)
+
+        assertThat(violations).isEmpty()
+    }
+
+    @Test
+    fun `allowOnlyStandardizedResponseCodes should return violation if non-standardized response code is used`() {
+        val context = withResponseCode("666")
+
+        val violations = rule.checkIfOnlyStandardizedResponseCodesAreUsed(context)
+
+        assertThat(violations).isNotEmpty
+        assertThat(violations[0].description).containsPattern(".*666 is not a standardized response code.*")
+        assertThat(violations[0].pointer.toString()).isEqualTo("/paths/~1pets/get/responses/666")
+    }
+
+    @Test
+    fun `checkIfOnlyWellUnderstoodResponseCodesAreUsed should return no violation if well-understood response code is used`() {
+        val context = withResponseCode("201")
+
+        val violations = rule.checkIfOnlyWellUnderstoodResponseCodesAreUsed(context)
+
+        assertThat(violations).isEmpty()
+    }
+
+    @Test
+    fun `checkIfOnlyWellUnderstoodResponseCodesAreUsed should return a violation if not well-understood response code is used`() {
+        val context = withResponseCode("417")
+
+        val violations = rule.checkIfOnlyWellUnderstoodResponseCodesAreUsed(context)
+
+        assertThat(violations).isNotEmpty
+        assertThat(violations[0].description).containsPattern(".*417 is not a well-understood response code.*")
+        assertThat(violations[0].pointer.toString()).isEqualTo("/paths/~1pets/get/responses/417")
+    }
+
+    private fun withResponseCode(responseCode: String): Context {
+        val content = """
+            openapi: '3.0.1'
+            paths:
+              /pets:
+                get:
+                  responses:
+                    $responseCode: {}
+            """.trimIndent()
+
+        return DefaultContext.createOpenApiContext(content)!!
     }
 }
