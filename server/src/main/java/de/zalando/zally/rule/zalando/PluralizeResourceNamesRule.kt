91e2ab09ef09dced71bed59040a6f0512b7d4d9f
Rob Oxspring <roxspring@imapmail.org>
2018-08-10 00:13:46 +0100

    feat(server): PluralizeResourceNamesRule operates with regex whitelist #780

--- server/src/main/java/de/zalando/zally/rule/zalando/PluralizeResourceNamesRule.kt
+++ server/src/main/java/de/zalando/zally/rule/zalando/PluralizeResourceNamesRule.kt
@@ -8,6 +8,7 @@ import de.zalando.zally.rule.api.Severity
 import de.zalando.zally.rule.api.Violation
 import de.zalando.zally.util.PatternUtil
 import de.zalando.zally.util.WordUtil.isPlural
+import io.swagger.v3.oas.models.PathItem
 import org.springframework.beans.factory.annotation.Autowired
 
 @Rule(
@@ -17,19 +18,33 @@ import org.springframework.beans.factory.annotation.Autowired
         title = "Pluralize Resource Names"
 )
 class PluralizeResourceNamesRule(@Autowired rulesConfig: Config) {
-    private val description = "Resource '%s' appears to be singular (but we are not sure)"
-    private val allowedPrefixes = rulesConfig.getConfig(javaClass.simpleName).getStringList("whitelist_prefixes")
+
+    internal val whitelist = mutableListOf(
+            *rulesConfig
+                    .getConfig(javaClass.simpleName)
+                    .getStringList("whitelist")
+                    .map { it.toRegex() }
+                    .toTypedArray())
 
     @Check(severity = Severity.MUST)
-    fun validate2(context: Context): List<Violation> {
+    fun validate(context: Context): List<Violation> {
         return context.validatePaths { (path, definition) ->
-
-            val allParts = path.split("/".toRegex())
-            val partsToCheck = if (allParts.size > 1 && allowedPrefixes.contains(allParts.first())) allParts.drop(1)
-            else allParts
-
-            partsToCheck.filter { s -> !s.isEmpty() && !PatternUtil.isPathVariable(s) && !isPlural(s) }
-                    .map { context.violation(String.format(description, it), definition) }
+            components(path)
+                    .filter { violatingComponent(it) }
+                    .map { violation(context, it, definition) }
         }
     }
+
+    fun components(path: String): List<String> {
+        var filtered = "/$path/"
+        "//+".toRegex().let { filtered = filtered.replace(it, "/") }
+        whitelist.forEach {filtered = filtered.replace(it, "/") }
+        return filtered.split("/+".toRegex()).filter { !it.isEmpty() }
+    }
+
+    private fun violatingComponent(it: String) =
+            !PatternUtil.isPathVariable(it) && !isPlural(it)
+
+    private fun violation(context: Context, term: String, definition: PathItem) =
+            context.violation("Resource '$term' appears to be singular", definition)
 }
--- server/src/test/java/de/zalando/zally/rule/zalando/PluralizeResourceNamesRuleTest.kt
+++ server/src/test/java/de/zalando/zally/rule/zalando/PluralizeResourceNamesRuleTest.kt
@@ -1,7 +1,9 @@
 package de.zalando.zally.rule.zalando
 
 import de.zalando.zally.getContextFromFixture
+import de.zalando.zally.rule.DefaultContext
 import de.zalando.zally.rule.ZallyAssertions.Companion.assertThat
+import de.zalando.zally.rule.api.Context
 import de.zalando.zally.testConfig
 import org.junit.Test
 
@@ -12,18 +14,18 @@ class PluralizeResourceNamesRuleTest {
     @Test
     fun positiveCase() {
         val context = getContextFromFixture("pluralizeResourcesValid.json")
-        val violations = rule.validate2(context)
+        val violations = rule.validate(context)
         assertThat(violations).isEmpty()
     }
 
     @Test
     fun negativeCase() {
         val context = getContextFromFixture("pluralizeResourcesInvalid.json")
-        val violations = rule.validate2(context)
+        val violations = rule.validate(context)
         assertThat(violations)
                 .descriptionsEqualTo(
-                        "Resource 'pet' appears to be singular (but we are not sure)",
-                        "Resource 'tail' appears to be singular (but we are not sure)")
+                        "Resource 'pet' appears to be singular",
+                        "Resource 'tail' appears to be singular")
                 .pointersEqualTo(
                         "/paths/~1pet~1cats",
                         "/paths/~1pets~1cats~1{cat-id}~1tail~1{tail-id}~1strands")
@@ -32,28 +34,28 @@ class PluralizeResourceNamesRuleTest {
     @Test
     fun positiveCaseSpp() {
         val context = getContextFromFixture("api_spp.json")
-        val violations = rule.validate2(context)
+        val violations = rule.validate(context)
         assertThat(violations).isEmpty()
     }
 
     @Test
     fun positiveCasePathsWithTheApiPrefix() {
         val context = getContextFromFixture("spp_with_paths_having_api_prefix.json")
-        val violations = rule.validate2(context)
+        val violations = rule.validate(context)
         assertThat(violations).isEmpty()
     }
 
     @Test
     fun positiveCaseNoMustViolations() {
         val context = getContextFromFixture("no_must_violations.yaml")
-        val violations = rule.validate2(context)
+        val violations = rule.validate(context)
         assertThat(violations).isEmpty()
     }
 
     @Test
     fun negativeCaseTinbox() {
         val context = getContextFromFixture("api_tinbox.yaml")
-        val violations = rule.validate2(context)
+        val violations = rule.validate(context)
         assertThat(violations)
                 .pointersEqualTo(
                         "/paths/~1meta~1article_domains",
@@ -65,5 +67,102 @@ class PluralizeResourceNamesRuleTest {
                         "/paths/~1queue~1models",
                         "/paths/~1queue~1models~1{model-id}",
                         "/paths/~1queue~1summaries")
+    }
+
+    @Test
+    fun `validate with plurals returns no violations`() {
+        val context = openApiContextWithPath("/plurals/things")
+
+        val violations = rule.validate(context)
+
+        assertThat(violations)
+                .isEmpty()
+    }
+
+    @Test
+    fun `validate with singulars returns violations`() {
+        val context = openApiContextWithPath("/singular/thing")
+
+        val violations = rule.validate(context)
+
+        assertThat(violations)
+                .descriptionsEqualTo(
+                        "Resource 'singular' appears to be singular",
+                        "Resource 'thing' appears to be singular")
+                .pointersEqualTo(
+                        "/paths/~1singular~1thing",
+                        "/paths/~1singular~1thing")
+    }
+
+    @Test
+    fun `validate with parameters returns other violations`() {
+        val context = openApiContextWithPath("/prefix/{parameter}/suffix")
+
+        val violations = rule.validate(context)
+
+        assertThat(violations)
+                .descriptionsEqualTo(
+                        "Resource 'prefix' appears to be singular",
+                        "Resource 'suffix' appears to be singular")
+                .pointersEqualTo(
+                        "/paths/~1prefix~1{parameter}~1suffix",
+                        "/paths/~1prefix~1{parameter}~1suffix")
+    }
+
+    @Test
+    fun `validate with configured whitelisted singulars returns no violations`() {
+        val context = openApiContextWithPath("/api/things")
+
+        val violations = rule.validate(context)
+
+        assertThat(violations)
+                .isEmpty()
+    }
+
+    @Test
+    fun `validate with whitelisted prefix returns no violations`() {
+        val context = openApiContextWithPath("/prefix/singular")
+        rule.whitelist += "^/?prefix/.*".toRegex()
+
+        val violations = rule.validate(context)
+
+        assertThat(violations)
+                .isEmpty()
+    }
+
+    @Test
+    fun `validate with whitelisted suffix returns no violations`() {
+        val context = openApiContextWithPath("/singular/suffix")
+        rule.whitelist += ".*/suffix/?${'$'}".toRegex()
+
+        val violations = rule.validate(context)
+
+        assertThat(violations)
+                .isEmpty()
+    }
+
+    @Test
+    fun `validate with whitelisted component returns other violations`() {
+        val context = openApiContextWithPath("/prefix/whitelisted/suffix")
+        rule.whitelist += "/whitelisted/".toRegex()
+
+        val violations = rule.validate(context)
+
+        assertThat(violations)
+                .descriptionsEqualTo(
+                        "Resource 'prefix' appears to be singular",
+                        "Resource 'suffix' appears to be singular")
+                .pointersEqualTo(
+                        "/paths/~1prefix~1whitelisted~1suffix",
+                        "/paths/~1prefix~1whitelisted~1suffix")
+    }
+
+    private fun openApiContextWithPath(path: String): Context {
+        val content = """
+                openapi: '3.0.0'
+                paths:
+                  $path: {}
+                """.trimIndent()
+        return DefaultContext.createOpenApiContext(content)!!
     }
 }
