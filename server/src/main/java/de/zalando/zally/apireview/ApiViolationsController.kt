4f963e206e72d15eb33991d8ab7c8f07dc782bb2
Rob Oxspring <roxspring@imapmail.org>
2018-08-14 22:22:53 +0100

    refactor(server): Convert de.zalando.zally.apireview to kotlin #712

---
+++ server/src/main/java/de/zalando/zally/apireview/ApiViolationsController.kt
@@ -0,0 +1,87 @@
+package de.zalando.zally.apireview
+
+import de.zalando.zally.dto.ApiDefinitionRequest
+import de.zalando.zally.dto.ApiDefinitionResponse
+import de.zalando.zally.dto.ViolationDTO
+import de.zalando.zally.dto.ViolationsCounter
+import de.zalando.zally.exception.InaccessibleResourceUrlException
+import de.zalando.zally.exception.MissingApiDefinitionException
+import de.zalando.zally.rule.ApiValidator
+import de.zalando.zally.rule.Result
+import de.zalando.zally.rule.RulesPolicy
+import de.zalando.zally.rule.api.Severity
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+@CrossOrigin
+@RestController
+class ApiViolationsController @Autowired
+constructor(
+    private val rulesValidator: ApiValidator,
+    private val apiDefinitionReader: ApiDefinitionReader,
+    private val apiReviewRepository: ApiReviewRepository,
+    private val serverMessageService: ServerMessageService,
+    private val configPolicy: RulesPolicy
+) {
+
+    @ResponseBody
+    @PostMapping("/api-violations")
+    fun validate(
+        @RequestBody(required = true) request: ApiDefinitionRequest,
+        @RequestHeader(value = "User-Agent", required = false) userAgent: String?
+    ): ApiDefinitionResponse {
+        val apiDefinition = retrieveApiDefinition(request)
+
+        val requestPolicy = retrieveRulesPolicy(request)
+
+        val violations = rulesValidator.validate(apiDefinition!!, requestPolicy)
+        apiReviewRepository.save(ApiReview(request, userAgent, apiDefinition, violations))
+
+        return buildApiDefinitionResponse(violations, userAgent)
+    }
+
+    private fun retrieveRulesPolicy(request: ApiDefinitionRequest): RulesPolicy = request.ignoreRules
+        ?.let { configPolicy.withMoreIgnores(request.ignoreRules!!) }
+        ?: configPolicy
+
+    private fun retrieveApiDefinition(request: ApiDefinitionRequest): String? = try {
+        apiDefinitionReader.read(request)
+    } catch (e: MissingApiDefinitionException) {
+        apiReviewRepository.save(ApiReview(request, null, ""))
+        throw e
+    } catch (e: InaccessibleResourceUrlException) {
+        apiReviewRepository.save(ApiReview(request, null, ""))
+        throw e
+    }
+
+    private fun buildApiDefinitionResponse(violations: List<Result>, userAgent: String?): ApiDefinitionResponse {
+        val response = ApiDefinitionResponse()
+        response.message = serverMessageService.serverMessage(userAgent)
+        response.violations = violations.map { this.toDto(it) }
+        response.violationsCount = buildViolationsCount(violations)
+        return response
+    }
+
+    private fun toDto(violation: Result): ViolationDTO = ViolationDTO(
+            violation.rule.title,
+            violation.description,
+            violation.violationType,
+            violation.ruleSet.url(violation.rule).toString(),
+            violation.paths,
+            if (violation.pointer == null) null else violation.pointer.toString()
+    )
+
+    private fun buildViolationsCount(violations: List<Result>): Map<String, Int> {
+        val counter = ViolationsCounter(violations)
+        return Severity.values()
+                .map { severity ->
+                    severity.name.toLowerCase() to counter[severity]
+                }
+                .toMap()
+    }
+}
---
+++ server/src/test/java/de/zalando/zally/apireview/RestApiTestConfiguration.kt
@@ -0,0 +1,67 @@
+package de.zalando.zally.apireview
+
+import com.fasterxml.jackson.core.JsonPointer
+import com.fasterxml.jackson.databind.JsonNode
+import de.zalando.zally.rule.TestRuleSet
+import de.zalando.zally.rule.api.Check
+import de.zalando.zally.rule.api.Context
+import de.zalando.zally.rule.api.Rule
+import de.zalando.zally.rule.api.Severity
+import de.zalando.zally.rule.api.Violation
+import de.zalando.zally.rule.zalando.UseOpenApiRule
+import de.zalando.zally.util.ast.JsonPointers
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
+
+@Configuration
+open class RestApiTestConfiguration {
+
+    @Bean
+    @Primary
+    @Profile("test")
+    open fun rules(): Collection<Any> {
+        return listOf(
+                TestCheckIsOpenApi3(),
+                TestCheckAlwaysReport3MustViolations(),
+                TestUseOpenApiRule()
+        )
+    }
+
+    /** Rule used for testing  */
+    @Rule(ruleSet = TestRuleSet::class, id = "TestCheckIsOpenApi3", severity = Severity.MUST, title = "TestCheckIsOpenApi3")
+    class TestCheckIsOpenApi3 {
+
+        @Check(severity = Severity.MUST)
+        fun validate(json: JsonNode): Violation? {
+            return if ("3.0.0" != json.path("openapi").textValue()) {
+                Violation("TestCheckIsOpenApi3", JsonPointer.compile("/openapi"))
+            } else null
+        }
+    }
+
+    /** Rule used for testing  */
+    @Rule(ruleSet = TestRuleSet::class, id = "TestCheckAlwaysReport3MustViolations", severity = Severity.MUST, title = "TestCheckAlwaysReport3MustViolations")
+    class TestCheckAlwaysReport3MustViolations {
+
+        @Check(severity = Severity.MUST)
+        fun validate(json: JsonNode): Iterable<Violation> {
+            return listOf(
+                    Violation("TestCheckAlwaysReport3MustViolations #1", JsonPointers.EMPTY),
+                    Violation("TestCheckAlwaysReport3MustViolations #2", JsonPointers.EMPTY),
+                    Violation("TestCheckAlwaysReport3MustViolations #3", JsonPointers.EMPTY)
+            )
+        }
+    }
+
+    /** Rule used for testing  */
+    @Rule(ruleSet = TestRuleSet::class, id = UseOpenApiRule.id, severity = Severity.MUST, title = "TestUseOpenApiRule")
+    class TestUseOpenApiRule {
+
+        @Check(severity = Severity.HINT)
+        fun validate(context: Context): Iterable<Violation> {
+            return emptyList()
+        }
+    }
+}
