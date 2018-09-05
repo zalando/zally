0ec035bd311a80c1f9328dab3cf428076c22beb1
Maxim Tschumak <maxim.tschumak@gmail.com>
2018-09-05 15:51:40 +0200

    fix(server): avoid NPE (#787)

---
+++ server/src/test/java/de/zalando/zally/util/OpenApiUtilTest.kt
@@ -0,0 +1,26 @@
+package de.zalando.zally.util
+
+import org.junit.Test
+
+class OpenApiUtilTest {
+
+    @Test
+    fun `getAllHeaders should return headers from components parameters`() {
+
+    }
+
+    @Test
+    fun `getAllHeaders should return headers from paths`() {
+
+    }
+
+    @Test
+    fun `getAllHeaders should return headers from compoents`() {
+
+    }
+
+    @Test
+    fun `getAllHeaders should `() {
+
+    }
+}
