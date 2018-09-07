9088cdb74dd0fee95cbe22afb1a033ff40047c80
Maxim Tschumak <maxim.tschumak@gmail.com>
2018-09-07 09:38:52 +0200

    feat(server): migrate no version in uri rul to use context object to support OpenAPI 3 (#714)

--- server/src/main/java/de/zalando/zally/rule/zalando/NoVersionInUriRule.kt
+++ server/src/main/java/de/zalando/zally/rule/zalando/NoVersionInUriRule.kt
@@ -1,24 +1,24 @@
 package de.zalando.zally.rule.zalando
 
 import de.zalando.zally.rule.api.Check
+import de.zalando.zally.rule.api.Context
 import de.zalando.zally.rule.api.Rule
 import de.zalando.zally.rule.api.Severity
 import de.zalando.zally.rule.api.Violation
 import de.zalando.zally.util.PatternUtil
-import io.swagger.models.Swagger
 
 @Rule(
-        ruleSet = ZalandoRuleSet::class,
-        id = "115",
-        severity = Severity.MUST,
-        title = "Do Not Use URI Versioning"
+    ruleSet = ZalandoRuleSet::class,
+    id = "115",
+    severity = Severity.MUST,
+    title = "Do Not Use URI Versioning"
 )
 class NoVersionInUriRule {
-    private val description = "basePath attribute contains version number"
+    private val description = "Server URL contains version number"
 
     @Check(severity = Severity.MUST)
-    fun validate(swagger: Swagger): Violation? {
-        val hasVersion = swagger.basePath != null && PatternUtil.hasVersionInUrl(swagger.basePath)
-        return if (hasVersion) Violation(description, emptyList()) else null
-    }
+    fun checkServerURLs(context: Context): List<Violation> =
+        context.api.servers.orEmpty()
+            .filter { PatternUtil.hasVersionInUrl(it.url) }
+            .map { context.violation(description, it) }
 }
--- server/src/test/java/de/zalando/zally/rule/zalando/NoVersionInUriRuleTest.kt
+++ server/src/test/java/de/zalando/zally/rule/zalando/NoVersionInUriRuleTest.kt
@@ -1,51 +1,43 @@
 package de.zalando.zally.rule.zalando
 
-import de.zalando.zally.rule.api.Violation
-import io.swagger.models.Swagger
+import de.zalando.zally.getOpenApiContextFromContent
 import org.assertj.core.api.Assertions.assertThat
+import org.intellij.lang.annotations.Language
 import org.junit.Test
 
 class NoVersionInUriRuleTest {
 
     private val rule = NoVersionInUriRule()
 
-    val expectedViolation = Violation(
-            "basePath attribute contains version number",
-            emptyList())
-
     @Test
-    fun returnsViolationsWhenVersionIsInTheBeginingOfBasePath() {
-        val swagger = Swagger().apply { basePath = "/v1/tests" }
-        assertThat(rule.validate(swagger)).isEqualTo(expectedViolation)
+    fun `checkServerURLs should return a violation if a server URL contains a version as base path`() {
+        @Language("YAML")
+        val spec = """
+            openapi: 3.0.1
+            servers:
+              - url: "https://inter.net/api/v1.0"
+        """.trimIndent()
+        val context = getOpenApiContextFromContent(spec)
+
+        val violations = rule.checkServerURLs(context)
+
+        assertThat(violations).isNotEmpty
+        assertThat(violations[0].description).contains("Server URL contains version number")
+        assertThat(violations[0].pointer.toString()).isEqualTo("/servers/0")
     }
 
     @Test
-    fun returnsViolationsWhenVersionIsInTheMiddleOfBasePath() {
-        val swagger = Swagger().apply { basePath = "/api/v1/tests" }
-        assertThat(rule.validate(swagger)).isEqualTo(expectedViolation)
-    }
+    fun `checkServerURLs should return no violations if a server URL does not contain a version as base path`() {
+        @Language("YAML")
+        val spec = """
+            openapi: 3.0.1
+            servers:
+              - url: "https://inter.net/api/"
+        """.trimIndent()
+        val context = getOpenApiContextFromContent(spec)
 
-    @Test
-    fun returnsViolationsWhenVersionIsInTheEndOfBasePath() {
-        val swagger = Swagger().apply { basePath = "/api/v1" }
-        assertThat(rule.validate(swagger)).isEqualTo(expectedViolation)
-    }
+        val violations = rule.checkServerURLs(context)
 
-    @Test
-    fun returnsViolationsWhenVersionIsBig() {
-        val swagger = Swagger().apply { basePath = "/v1024/tests" }
-        assertThat(rule.validate(swagger)).isEqualTo(expectedViolation)
-    }
-
-    @Test
-    fun returnsEmptyViolationListWhenNoVersionFoundInURL() {
-        val swagger = Swagger().apply { basePath = "/violations/" }
-        assertThat(rule.validate(swagger)).isNull()
-    }
-
-    @Test
-    fun returnsEmptyViolationListWhenBasePathIsNull() {
-        val swagger = Swagger()
-        assertThat(rule.validate(swagger)).isNull()
+        assertThat(violations).isEmpty()
     }
 }
