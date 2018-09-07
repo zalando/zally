3748257c01643f186ee75677d1db6a9d9e3d9163
Maxim Tschumak <maxim.tschumak@gmail.com>
2018-09-07 09:14:29 +0200

    feat(server): migrate nested paths may be root paths rule to use context object to support OpenAPI 3 (#714)

--- server/src/main/java/de/zalando/zally/rule/zalando/NestedPathsMayBeRootPathsRule.kt
+++ server/src/main/java/de/zalando/zally/rule/zalando/NestedPathsMayBeRootPathsRule.kt
@@ -1,28 +1,25 @@
 package de.zalando.zally.rule.zalando
 
 import de.zalando.zally.rule.api.Check
+import de.zalando.zally.rule.api.Context
 import de.zalando.zally.rule.api.Rule
 import de.zalando.zally.rule.api.Severity
 import de.zalando.zally.rule.api.Violation
 import de.zalando.zally.util.PatternUtil.isPathVariable
-import io.swagger.models.Swagger
 
 @Rule(
-        ruleSet = ZalandoRuleSet::class,
-        id = "145",
-        severity = Severity.MAY,
-        title = "Consider Using (Non-) Nested URLs"
+    ruleSet = ZalandoRuleSet::class,
+    id = "145",
+    severity = Severity.MAY,
+    title = "Consider Using (Non-) Nested URLs"
 )
 class NestedPathsMayBeRootPathsRule {
-    private val description = "Nested paths / URLs may be top-level resource"
+    private val description = "Nested paths may be top-level resource"
 
     @Check(severity = Severity.MAY)
-    fun validate(swagger: Swagger): Violation? {
-        val paths = swagger.paths.orEmpty().keys.filter {
-            val pathSegments = it.split("/".toRegex())
-            // we are only interested in paths that have sub-resource followed by a param: /path1/{param1}/path2/{param2}
-            pathSegments.size > 4 && isPathVariable(pathSegments.last())
-        }
-        return if (paths.isNotEmpty()) Violation(description, paths) else null
-    }
+    fun checkNestedPaths(context: Context): List<Violation> =
+        context.api.paths.orEmpty().entries
+            .map { (path, pathEntry) -> Pair(pathEntry, path.split("/").filter { isPathVariable(it) }.count()) }
+            .filter { (_, numberOfPathParameters) -> numberOfPathParameters > 1 }
+            .map { (path, _) -> context.violation(description, path) }
 }
