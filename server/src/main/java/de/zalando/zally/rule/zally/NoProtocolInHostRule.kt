66469c0f06b43f71c99f237868144ab8be34a5dd
Rob Oxspring <roxspring@imapmail.org>
2018-09-26 13:35:07 +0100

    feat(server): NoProtocolInHostRule operates on Context but only Swagger specs #834

--- server/src/main/java/de/zalando/zally/rule/zally/NoProtocolInHostRule.kt
+++ server/src/main/java/de/zalando/zally/rule/zally/NoProtocolInHostRule.kt
@@ -1,11 +1,22 @@
 package de.zalando.zally.rule.zally
 
+import com.fasterxml.jackson.core.JsonPointer
 import de.zalando.zally.rule.api.Check
+import de.zalando.zally.rule.api.Context
 import de.zalando.zally.rule.api.Rule
 import de.zalando.zally.rule.api.Severity
 import de.zalando.zally.rule.api.Violation
 import io.swagger.models.Swagger
 
+/**
+ * Validates that [Swagger.host] does not contain a url scheme as required by
+ * [OpenAPI 2.0](https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#swagger-object).
+ *
+ * > The host (name or ip) serving the API. This MUST be the host only and does not include the scheme nor sub-paths.
+ *
+ * Note that this rule operates on a [Context] but specifically targets [Swagger.host] since the OpenAPI 3.0.0
+ * equivalent [io.swagger.v3.oas.models.servers.Server.url] is a url rather than "host only".
+ */
 @Rule(
         ruleSet = ZallyRuleSet::class,
         id = "M008",
@@ -13,13 +24,15 @@ import io.swagger.models.Swagger
         title = "Host should not contain protocol"
 )
 class NoProtocolInHostRule {
-    private val desc = "Information about protocol should be placed in schema. Current host value '%s' violates this rule"
 
     @Check(severity = Severity.MUST)
-    fun validate(swagger: Swagger): Violation? {
-        val host = swagger.host.orEmpty()
-        return if ("://" in host)
-            Violation(desc.format(host), emptyList())
-        else null
+    fun validate(context: Context): List<Violation> {
+        val host = context.swagger?.host.orEmpty()
+        return when {
+            "://" in host -> listOf(Violation(
+                "'$host' contains protocol information which should be listed separately as schemes",
+                JsonPointer.compile("/host")))
+            else -> emptyList()
+        }
     }
 }
