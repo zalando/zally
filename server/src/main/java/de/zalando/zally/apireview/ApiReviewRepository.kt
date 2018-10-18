f44292a2c92f2a27f17f50a70f41d654485189d6
Maxim Tschumak <maxim.tschumak@gmail.com>
2018-10-18 11:51:39 +0200

    feat(server): exclude violations and full reviews from the statistics endpoint (#873)

--- server/src/main/java/de/zalando/zally/apireview/ApiReviewRepository.kt
+++ server/src/main/java/de/zalando/zally/apireview/ApiReviewRepository.kt
@@ -1,11 +1,47 @@
 package de.zalando.zally.apireview
 
+import de.zalando.zally.statistic.ReviewStatistics
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import java.time.LocalDate
 
 interface ApiReviewRepository : CrudRepository<ApiReview, Long> {
 
-    fun findByDayBetween(from: LocalDate, to: LocalDate): Collection<ApiReview>
+    @Query("""
+        SELECT new de.zalando.zally.statistic.ReviewStatistics(
+	        COUNT(r) AS totalReviews,
+	        COUNT(DISTINCT r.name) AS totalReviewDeduplicated,
+	        COALESCE(SUM(CASE WHEN r.isSuccessfulProcessed = 'True' THEN 1 ELSE 0 END),0) AS successfulReviews,
+	        COALESCE(SUM(r.numberOfEndpoints),0) AS numberOfEndpoints,
+	        COALESCE(SUM(r.mustViolations),0) AS mustViolations,
+	        COALESCE(SUM(r.shouldViolations),0) AS shouldViolations,
+	        COALESCE(SUM(r.mayViolations),0) AS mayViolations,
+	        COALESCE(SUM(r.hintViolations),0) AS hintViolations)
+        FROM de.zalando.zally.apireview.ApiReview r
+        WHERE r.day >= :from AND r.day <= :to
+    """)
+    fun getReviewStatistics(
+        @Param("from") from: LocalDate,
+        @Param("to") to: LocalDate
+    ): ReviewStatistics
 
-    fun findByUserAgentAndDayBetween(userAgent: String, from: LocalDate, to: LocalDate): Collection<ApiReview>
+    @Query("""
+        SELECT new de.zalando.zally.statistic.ReviewStatistics(
+	        COUNT(r) AS totalReviews,
+	        COUNT(DISTINCT r.name) AS totalReviewDeduplicated,
+	        COALESCE(SUM(CASE WHEN r.isSuccessfulProcessed = 'True' THEN 1 ELSE 0 END),0) AS successfulReviews,
+	        COALESCE(SUM(r.numberOfEndpoints),0) AS numberOfEndpoints,
+	        COALESCE(SUM(r.mustViolations),0) AS mustViolations,
+	        COALESCE(SUM(r.shouldViolations),0) AS shouldViolations,
+	        COALESCE(SUM(r.mayViolations),0) AS mayViolations,
+	        COALESCE(SUM(r.hintViolations),0) AS hintViolations)
+        FROM de.zalando.zally.apireview.ApiReview r
+        WHERE user_agent = :userAgent AND day >= :from AND day <= :to
+    """)
+    fun getReviewStatisticsByUserAgent(
+        @Param("userAgent") userAgent: String,
+        @Param("from") from: LocalDate,
+        @Param("to") to: LocalDate
+    ): ReviewStatistics
 }
