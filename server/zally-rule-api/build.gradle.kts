21c3a9d518245d8af634d6ae4df8c8cf016bd3cf
Rob Oxspring <roxspring@imapmail.org>
2019-03-28 22:01:52 +0000

    chore(server): Gradle scripts use Kotlin DSL #969

---
+++ server/zally-rule-api/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    compile("io.swagger.core.v3:swagger-models:2.0.1")
+    compile("io.swagger:swagger-models:1.5.2")
+    compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.9+")
+
+    testCompile("junit:junit:4.12")
+}
