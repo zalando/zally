a2225a14763fa1a801a82c1805c8ea06495f9547
Vadim <5587834+vadeg@users.noreply.github.com>
2019-12-23 17:10:20 +0100

    Remove `spring` dependency from `RulesPolicy`
    Convert `RulesPolicy` to Kotlin data class and remove dependency from `spring` annotations.
    
    Fixes: #560

--- server/src/main/java/de/zalando/zally/rule/RulesPolicy.kt
+++ server/src/main/java/de/zalando/zally/rule/RulesPolicy.kt
@@ -1,15 +1,27 @@
 package de.zalando.zally.rule
 
 import de.zalando.zally.rule.api.Rule
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Component
 
-@Component
-class RulesPolicy(@Value("\${zally.ignoreRules:}") val ignoreRules: Array<String>) {
+data class RulesPolicy(val ignoreRules: Array<String>) {
 
     fun accepts(rule: Rule): Boolean {
         return !ignoreRules.contains(rule.id)
     }
 
-    fun withMoreIgnores(moreIgnores: List<String>) = RulesPolicy(ignoreRules + moreIgnores)
+    fun withMoreIgnores(moreIgnores: List<String>) = this.copy(ignoreRules = ignoreRules + moreIgnores)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as RulesPolicy
+
+        if (!ignoreRules.contentEquals(other.ignoreRules)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return ignoreRules.contentHashCode()
+    }
 }
