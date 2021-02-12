577408f9e8dbb9f6ac60739396f12bcd5d3d8951
server-may-cry <sopletaev@gmail.com>
2021-02-12 13:13:48 +0100

    Add "self" as an exception for the pluralization rules

--- server/zally-ruleset-zalando/src/main/kotlin/org/zalando/zally/ruleset/zalando/util/WordUtil.kt
+++ server/zally-ruleset-zalando/src/main/kotlin/org/zalando/zally/ruleset/zalando/util/WordUtil.kt
@@ -3,7 +3,7 @@ package org.zalando.zally.ruleset.zalando.util
 import javatools.parsers.PlingStemmer
 
 internal object WordUtil {
-    private val PLURAL_WHITELIST = setOf("vat", "apis")
+    private val PLURAL_WHITELIST = setOf("vat", "apis", "self")
 
     fun isPlural(word: String): Boolean = PLURAL_WHITELIST.contains(word) || PlingStemmer.isPlural(word)
 }
