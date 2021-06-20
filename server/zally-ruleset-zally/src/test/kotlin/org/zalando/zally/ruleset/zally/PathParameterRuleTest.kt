5eb6f4bd55d2659921012904029431d8c37c2f43
Vadim <5587834+vadeg@users.noreply.github.com>
2021-06-20 21:55:32 +0200

    Add a test with parameter defined in `components` section

--- server/zally-ruleset-zally/src/test/kotlin/org/zalando/zally/ruleset/zally/PathParameterRuleTest.kt
+++ server/zally-ruleset-zally/src/test/kotlin/org/zalando/zally/ruleset/zally/PathParameterRuleTest.kt
@@ -77,4 +77,44 @@ class PathParameterRuleTest {
         val violations = rule.validate(context)
         assertThat(violations).isEmpty()
     }
+
+    @Test
+    internal fun `return violation if a path parameter in components is not marked as "required"`() {
+        @Language("YAML")
+        val context = DefaultContextFactory().getOpenApiContext(
+            """
+            openapi: '3.0.0'
+            info:
+              title: API 1
+              contact: 
+                info: "Team One"               
+            paths:
+              /items/{item-id}:
+                get:
+                  parameters:
+                    - $\ref: "#/components/parameters/QueryParameter"
+                  responses: 
+                    default:
+                     description: Response
+                     content:                        
+                       application/json:
+                         schema: 
+                           type: string
+            components: 
+              parameters:
+                QueryParameter:
+                  name: item-id
+                  in: path                  
+                  description: The id of the pet to retrieve
+                  schema:
+                    type: string
+            """.trimIndent()
+        )
+
+        val violations = rule.validate(context)
+
+        assertThat(violations)
+            .descriptionsAllEqualTo(PathParameterRule.ERROR_MESSAGE)
+            .pointersEqualTo("/components/parameters/QueryParameter")
+    }
 }
