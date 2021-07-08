af5090afe247074fef60d42da04b8362ef126abd
Vadim <5587834+vadeg@users.noreply.github.com>
2021-07-08 21:17:55 +0200

    Update rules annotation processor to Java 11

--- server/zally-core/src/main/kotlin/org/zalando/zally/core/RuleProcessor.kt
+++ server/zally-core/src/main/kotlin/org/zalando/zally/core/RuleProcessor.kt
@@ -11,7 +11,7 @@ import javax.lang.model.element.TypeElement
 import javax.tools.StandardLocation
 
 @AutoService(Processor::class)
-@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedSourceVersion(SourceVersion.RELEASE_11)
 class RuleProcessor : AbstractProcessor() {
 
     val annotation = Rule::class.java
