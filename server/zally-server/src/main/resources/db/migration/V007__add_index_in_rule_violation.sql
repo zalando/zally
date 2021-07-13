eb75e94d637eea31bb8a2e386771622af72f25a7
Vadim <5587834+vadeg@users.noreply.github.com>
2021-07-13 21:12:37 +0200

    Adapt SQL syntax to SQL92

--- server/zally-server/src/main/resources/db/migration/V007__add_index_in_rule_violation.sql
+++ server/zally-server/src/main/resources/db/migration/V007__add_index_in_rule_violation.sql
@@ -1,1 +1,1 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS rule_violation_api_review_id_idx ON rule_violation(api_review_id);
+CREATE INDEX IF NOT EXISTS rule_violation_api_review_id_idx ON rule_violation(api_review_id);
