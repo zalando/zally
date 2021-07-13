f3d45a3bb4079aa68e201d833e4fb07fb30b0917
Vadim <5587834+vadeg@users.noreply.github.com>
2021-07-13 18:04:46 +0200

    Update index query

--- server/zally-server/src/main/resources/db/migration/V007__add_index_in_rule_violation.sql
+++ server/zally-server/src/main/resources/db/migration/V007__add_index_in_rule_violation.sql
@@ -1,1 +1,1 @@
-CREATE INDEX rule_violation_api_review_id_idx ON rule_violation (api_review_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS rule_violation_api_review_id_idx ON rule_violation(api_review_id);
