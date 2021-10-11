e653dd46998e54f110a54369dd1e488f74a1cb05
Jaqueline Schweigert <jaqueline.pollak@gmail.com>
2021-10-11 09:02:12 +0200

    Use BOM approach for junit depnendency

--- server/zally-rule-api/build.gradle.kts
+++ server/zally-rule-api/build.gradle.kts
@@ -3,6 +3,7 @@ dependencies {
     implementation("io.swagger:swagger-models:1.6.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.2")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation(platform("org.junit:junit-bom:5.8.1"))
+	testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.11.0")
 }
