8c403057dd4b56e6e434200a6aac91c75f4ea541
tkrop <tronje.krop@zalando.de>
2022-01-03 13:54:11 +0100

    fix: correct imports and syntax (#1318)

--- server/zally-rule-api/build.gradle.kts
+++ server/zally-rule-api/build.gradle.kts
@@ -4,6 +4,6 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.2")
 
     testImplementation(platform("org.junit:junit-bom:5.8.1"))
-	testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.11.0")
 }
