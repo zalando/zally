a9b01cd44355e7b86fa744bc19975f6f928fd66c
Max Doesburg <MDoesburg@linz.govt.nz>
2022-05-06 15:32:27 +1200

    Disable oauth2 in prod profile.

--- server/zally-server/src/main/kotlin/org/zalando/zally/configuration/NoSecurityConfiguration.kt
+++ server/zally-server/src/main/kotlin/org/zalando/zally/configuration/NoSecurityConfiguration.kt
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
-@Profile("dev", "test")
+@Profile("dev", "test", "production")
 @Configuration
 class NoSecurityConfiguration : WebSecurityConfigurerAdapter() {
 
