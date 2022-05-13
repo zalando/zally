67c7a5ad7570e120d9c5dc11efbbbcbbb2dcedd8
Bala Paranj <bparanj@gmail.com>
2022-05-13 09:33:14 -0400

    Create build-and-run-linode.sh
    Build and run script for Linode instance

---
+++ build-and-run-linode.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# This script starts the Zally components via docker-compose:
+#  the server component is separately built inside its own container.
+
+
+set -ex
+
+hash yarn 2>/dev/null || { echo >&2 "yarn is not installed.."; exit 1; }
+
+pushd $(dirname $0) > /dev/null
+SCRIPT_DIR=$(pwd -P)
+popd > /dev/null
+
+SERVER_DIR=${SCRIPT_DIR}/server
+WEB_UI_DIR=${SCRIPT_DIR}/web-ui
+
+# Build web ui
+cd ${WEB_UI_DIR} && yarn && yarn build
+
+# Docker-compose
+cd ${SCRIPT_DIR} && docker-compose up --build
