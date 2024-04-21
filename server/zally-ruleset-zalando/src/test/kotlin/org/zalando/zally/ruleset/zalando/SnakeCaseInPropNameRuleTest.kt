ed6a2e336299c720a9c98dc44475e1b7a10437f3
theo <theo.liu@kompasbank.dk>
2024-04-21 13:21:27 +0000

    add import

--- server/zally-ruleset-zalando/src/test/kotlin/org/zalando/zally/ruleset/zalando/SnakeCaseInPropNameRuleTest.kt
+++ server/zally-ruleset-zalando/src/test/kotlin/org/zalando/zally/ruleset/zalando/SnakeCaseInPropNameRuleTest.kt
@@ -3,6 +3,7 @@ package org.zalando.zally.ruleset.zalando
 import org.assertj.core.api.Assertions.assertThat
 import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Disabled
 import org.zalando.zally.core.DefaultContextFactory
 import org.zalando.zally.core.rulesConfig
 
